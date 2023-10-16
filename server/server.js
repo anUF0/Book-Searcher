7d25d18303b47eb7c50b398f8e79d5aeaf0c7a91
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-10-17 09:44:53 +1100

    Updated auth and server to take context middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('apollo/server/express4');
 const { typeDefs, resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,7 +19,7 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('./graphql', expressMiddleware(server));
+  app.use('./graphql', expressMiddleware(server, { context: authMiddleware }));
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
