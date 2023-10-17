350fbbe22147a58fe0f8a1cf62bd9845c140ff75
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-10-17 14:50:15 +1100

    Fixed major syntax error in mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,56 +1,63 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login({$or: username: String! ,email: String!}, password: String!){
-    token
-    user{
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
         _id
         username
+      }
     }
-    }
+  }
 `;
 
 export const ADD_USER = gql`
-    mutation addUser(username: String!, email: String!, password: String!){
-    token
-    user {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
         _id
         username
+      }
     }
-    }`;
-
-export const SAVE_BOOK = gql`
-    mutation saveBooksaveBook( {bookId: ID!, authors: String, description: String!, image: String, link: String, title: String!}){
-        {
-        _id
-        username
-        email
-        savedBooks {
-            bookId
-            authors
-            title
-            description
-            image
-            link
-        }
-        bookCount
-        }
-    }
+  }
 `;
 
-export const REMOVE_BOOK = gql`({bookId: ID!}){
-            {
-        _id
-        username
-        email
-        savedBooks {
-            bookId
-            authors
-            title
-            description
-            image
-            link
-        }
-        bookCount
-        }
-}`;
+export const SAVE_BOOK = gql`
+  mutation saveBooksaveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+      bookCount
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+      bookCount
+    }
+  }
+`;
